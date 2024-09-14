11ae3a6c39f68df5a75b66ab55d6368ce9df9ced
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-14 13:35:13 -0400

    init file

---
+++ WordFrequenciesTwo.java
@@ -0,0 +1,9 @@
+package WordFrequenciesTwo;
+
+import java.util.Scanner; 
+
+public class WordFrequenciesTwo {
+   public static void main(String[] args) {
+      /* Type your code here. */
+   }
+}
