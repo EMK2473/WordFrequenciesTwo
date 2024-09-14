d54d6644504bb90996503bf5ab81fe471408568d
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-14 14:22:38 -0400

    declared and assigned variables; assigned input to string array; split array at spaces; parsed int at index 0 set as num of elemets; looped through stringArray and printed out string values for testing

--- WordFrequenciesTwo.java
+++ WordFrequenciesTwo.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class WordFrequenciesTwo {
    public static void main(String[] args) {
-      /* Type your code here. */
+      Scanner scnr = new Scanner(System.in);
+      
+      String input = scnr.nextLine();
+      String[] stringArray = input.split(" ");
+      int NUM_ELEMENTS = Integer.parseInt(stringArray[0]);
+
+      System.out.println(NUM_ELEMENTS);
+
+      for(int i = 1; i < stringArray.length; ++i) {
+        System.out.println(stringArray[i]);
+      }
+
+
+
+
+      scnr.close();
    }
 }
